69de6221c9065100dedcabb543e51b937a5ac51f
sathyaprakash195 <sathyaprakash195@gmail.com>
2022-07-15 14:48:11 +0530

    first commit

---
+++ cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: "sathya195",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+module.exports = {cloudinary};
